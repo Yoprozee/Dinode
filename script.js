7d3e75777bf9892ff0c451a77d8ab31d8db5b7d3
Yoprozee <kelvink1adeyeye@gmail.com>
2022-08-08 10:49:30 +0100

    Fixed css style issue

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+
+window.onscroll = function() {stickBar()};
+
+// Get the navbar
+var navbar = document.getElementById("topnavbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickBar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
 function hideId(id) {
 	document.getElementById(id).style.display = "none";
 }
